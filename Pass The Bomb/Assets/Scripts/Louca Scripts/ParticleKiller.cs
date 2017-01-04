9457acad8390947eed48505f18ab804217f7c1ac
LoucaColes <coleslouca@yahoo.co.uk>
2017-01-04 16:06:01 +0000

    Particle Effects
    Added a explosion effect (obviously not final) that we can use. I think
    the style suits the games aesthetics, will just need to modify
    parameters if anything.
    Also added a particle spawner and killer script. The killer script takes
    a float and will destroy the game object after the float amount of
    seconds. The spawner atm will only spawn in testing mode. But will
    eventually have a public function to spawn the particle which will then
    be called from other scripts when required.

---
+++ Pass The Bomb/Assets/Scripts/Louca Scripts/ParticleKiller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleKiller : MonoBehaviour {
+
+    public float m_lifeTime;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, m_lifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
