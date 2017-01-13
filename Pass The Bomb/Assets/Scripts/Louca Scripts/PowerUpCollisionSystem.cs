e6904f8d21fa79eabd331e321543f67beb710a67
LoucaColes <Louca2.Coles@live.uwe.ac.uk>
2017-01-13 11:07:07 +0000

    Base power up system
    Base power up system set up, audio not working yet, possibly for the
    same reason as the last game jam.

---
+++ Pass The Bomb/Assets/Scripts/Louca Scripts/PowerUpCollisionSystem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpCollisionSystem : MonoBehaviour {
+
+    private AudioSource m_audioSource;
+    private ParticleSpawner m_particleSpawner;
+    public AudioClip m_audioClip;
+    public Camera m_camera;
+
+	// Use this for initialization
+	void Start () {
+        m_audioSource = gameObject.GetComponent<AudioSource>();
+        m_particleSpawner = gameObject.GetComponent<ParticleSpawner>();
+        m_audioSource.clip = m_audioClip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "Player")
+        {
+            m_particleSpawner.SpawnParticle();
+            gameObject.transform.position = m_camera.transform.position;
+            PlayAudio();
+        }
+    }
+
+    void PlayAudio()
+    {
+        m_audioSource.Play();
+    }
+}
---
+++ Pass The Bomb/Assets/Scripts/Louca Scripts/PowerUpSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PowerUpSpawner : MonoBehaviour {
+
+    public Transform[] m_spawnPoint;
+    public GameObject m_powerUp;
+    public List<GameObject> m_SpawnedPowerUps;
+
+	// Use this for initialization
+	void Start () {
+        SpawnPowerUps();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SpawnPowerUps()
+    {
+        for (int i = 0; i < m_spawnPoint.Length; i++)
+        {
+            GameObject m_tempGameObjects = (GameObject)Instantiate(m_powerUp, m_spawnPoint[i].transform.position, m_spawnPoint[i].transform.rotation);
+        }
+    }
+}
