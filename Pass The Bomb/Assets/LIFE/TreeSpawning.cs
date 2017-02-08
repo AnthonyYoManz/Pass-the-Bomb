c4115af5d7d7d759f459f733cde686b9e526e4f1
d2-pamphilon <danpamphilon@yahoo.co.uk>
2017-02-08 12:03:26 +0000

    tree spawning and bunny

---
+++ Pass The Bomb/Assets/LIFE/TreeSpawning.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeSpawning : MonoBehaviour
+{
+   // public Transform[] children;
+    public GameObject tree;
+    public GameObject tree2;
+    void Start()
+    {
+
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.name == "SSpawn")
+            {
+                Instantiate(tree, child.position, child.rotation);
+            }
+            else if (child.gameObject.name == "CSpawn")
+            {
+                Instantiate(tree2, child.position, child.rotation);
+            }
+
+            //child is your child transform
+        }
+    }
+
+
+    void Update()
+    {
+
+    }
+}
