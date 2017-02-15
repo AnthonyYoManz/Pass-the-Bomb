b732338683b4b0ded72bb5bb68667ab395f32ac3
d2-pamphilon <danpamphilon@yahoo.co.uk>
2017-02-15 11:29:33 +0000

    spawning asorted things
    trees, rock, signs, lamps

--- Pass The Bomb/Assets/LIFE/Scripts/TreeSpawning.cs
+++ Pass The Bomb/Assets/LIFE/Scripts/TreeSpawning.cs
@@ -8,21 +8,48 @@ namespace GCSharp
         // public Transform[] children;
         public GameObject tree;
         public GameObject tree2;
+        public GameObject sign;
+        public GameObject sign2;
+        public GameObject lamp;
+        public GameObject rock;
+
+        public Vector3 rockShift;
+        public Vector3 treeShift;
+        public Vector3 lampShift;
+        public Vector3 signShift;
+
         void Start()
         {
 
             foreach (Transform child in transform)
             {
-                if (child.gameObject.name == "SSpawn")
+                switch (child.gameObject.name)
                 {
-                    Instantiate(tree, child.position, child.rotation);
+                    case "SSpawn":
+                        Instantiate(tree, child.position += treeShift, child.rotation);
+                        break;
+                    case "CSpawn":
+                        Instantiate(tree2, child.position += treeShift, child.rotation);
+                        break;
+                    case "streetSign":
+                        if (Random.Range(0f, 1f) >= 0.5f)
+                        {
+                            Instantiate(sign, child.position += signShift, child.rotation);
+                        }
+                        else
+                        {
+                            Instantiate(sign2, child.position += signShift, child.rotation);
+                        }
+                        break;
+                    case "lamp":
+                        Instantiate(lamp, child.position += lampShift, child.rotation);
+                        break;
+                    case "rock":
+                        Instantiate(rock, child.position += rockShift, child.rotation);
+                        break;
+                    default:
+                        break;
                 }
-                else if (child.gameObject.name == "CSpawn")
-                {
-                    Instantiate(tree2, child.position, child.rotation);
-                }
-
-                //child is your child transform
             }
         }
 
